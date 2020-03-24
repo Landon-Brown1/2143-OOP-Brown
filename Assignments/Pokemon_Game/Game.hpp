44d7ce5b3896b7501b81a0778a2e9702e8248659
Landon Brown <lmbrown0908@my.msutexas.edu>
2020-03-23 20:38:29 -0500

    created base files for game

---
+++ Assignments/Pokemon_Game/Game.hpp
@@ -0,0 +1,23 @@
+/*
+* AUTHOR: Landon Brown
+* FILE TITLE: Game.hpp
+* FILE DESCRIPTION: Build a Pokémon game class. The main function
+*   of this game is to serve as a Pokémon battle simulation.
+* DUE DATE: TBD
+* DATE CREATED: 03/14/2020 
+*/
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class Game{
+    private:
+        
+
+    public:
+
+
+};
